b3eaf0712867e137b68b25cfce512e6751c6f48f
finxcode <f.sheng@outlook.com>
2023-06-07 22:47:31 +0800

    modify data entity fields

--- adapter/out/entity/quote.go
+++ adapter/out/entity/quote.go
@@ -3,9 +3,9 @@ package entity
 import "time"
 
 type Quote struct {
-	CoinId           int       `json:"coin_Id"`
-	Symbol           string    `json:"symbol"`
-	PercentChange24H float64   `json:"percentChange24H"`
-	PercentChange7D  float64   `json:"percentChange7D"`
-	LastUpdated      time.Time `json:"last_updated"`
+	CoinId           int       `json:"coin_id" db:"coin_id"`
+	Symbol           string    `json:"symbol" db:"symbol"`
+	PercentChange24H float64   `json:"percentChange24H" db:"percentChange24H"`
+	PercentChange7D  float64   `json:"percentChange7D" db:"percentChange7D"`
+	LastUpdated      time.Time `json:"last_updated" db:"last_updated"`
 }
