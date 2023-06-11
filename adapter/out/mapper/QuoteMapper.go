c92e5a1c4f9501d2253f98ad26341b48b83976eb
finxcode <f.sheng@outlook.com>
2023-06-11 20:18:11 +0800

    correct quote mapper misspell

--- adapter/out/mapper/QuoteMapper.go
+++ adapter/out/mapper/QuoteMapper.go
@@ -6,6 +6,6 @@ import (
 )
 
 func QuoteDataToDomain(quoteData *dataEntity.Quote, tradingDay string) *entity.Quote {
-	return entity.NewQuote(quoteData.CoinId, quoteData.Symbol, quoteData.PercentChange7D,
+	return entity.NewQuote(quoteData.CoinId, quoteData.Symbol, quoteData.PercentChange24H,
 		quoteData.PercentChange7D, tradingDay)
 }
